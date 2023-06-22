f1acaa94b7f14bfed9d9a3b3063936134641459d
Prateek Vijay <113239008+pkvijay65@users.noreply.github.com>
2023-06-22 19:07:27 +0530

    change col name to unreserved word

--- src/main/java/com/project/bookmyshow/models/Seat.java
+++ src/main/java/com/project/bookmyshow/models/Seat.java
@@ -1,14 +1,20 @@
 package com.project.bookmyshow.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Seat extends BaseModel {
   private String seatNumber;
+  @Column(name = "r_number")
   private int rowNumber;
+  @Column(name = "c_number")
   private int columnNumber;
+  @ManyToOne
   private SeatType seatType;
-
 }
